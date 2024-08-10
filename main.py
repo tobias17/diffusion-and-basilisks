45ff6b34ed31e2e009785a4bbe47aad23b0cc503
tobias17 <tobiasfischer17@gmail.com>
2024-08-10 14:28:05 -0400

    more api design

--- main.py
+++ main.py
@@ -16,8 +16,29 @@ def list_hubs() -> List[HubDescription]:
 def create_hub(hub_name:str, hub_description) -> None:
    pass
 
-def create_npc(hub_name:str, character_name:str, character_description:str, physical_description:str) -> None:
+
+class NpcDescription: pass
+def list_npcs() -> List[NpcDescription]:
+   return []
+
+def create_npc(character_name:str, character_description:str, physical_description:str) -> None:
    pass
+
+def talk_to_npc(character_name:str) -> None:
+   pass
+
+
+class ItemDescription: pass
+def list_player_items() -> List[ItemDescription]:
+   return []
+
+def give_player_item(item_name:str, item_description:str, physical_description:str) -> None:
+   pass
+
+def player_use_item(item_name:str, action_description:str, consume_amount:int=0) -> None:
+   pass
+
+
 
 def main():
    pass
