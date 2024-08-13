6d88baf132a70f5662f849bc6d077bdd0be77f7c
tobias17 <tobiasfischer17@gmail.com>
2024-08-13 19:48:19 -0400

    functions organization

--- main.py
+++ main.py
@@ -1,50 +1,8 @@
 from common import State, Event, StateTransitionEvent
 from prompts import Template
+from functions import Functions
 
-from typing import List, Dict
-
-
-class Functions:
-   state_to_methods_map: Dict[State,List] = {}
-   @staticmethod
-   def register(fxn, *states:State):
-      for state in states:
-         if state not in Functions.state_to_methods_map:
-            Functions.state_to_methods_map[state] = []
-         Functions.state_to_methods_map[state].append(fxn)
-
-class HubDescription: pass
-def list_hubs() -> List[HubDescription]:
-   return []
-Functions.register(list_hubs, State.HUB_IDLE, State.HUB_TALKING, State.TRAVEL_IDLE, State.TRAVEL_TALK)
-
-def create_hub(hub_name:str, hub_description) -> None:
-   pass
-Functions.register(list_hubs, State.HUB_IDLE, State.HUB_TALKING, State.TRAVEL_IDLE, State.TRAVEL_TALK)
-
-
-class NpcDescription: pass
-def list_npcs() -> List[NpcDescription]:
-   return []
-
-def create_npc(character_name:str, character_description:str, physical_description:str) -> None:
-   """Creates a new NPC with the given properties."""
-
-def talk_to_npc(character_name:str) -> None:
-   """Initiates a converstation between the player and the specified NPC."""
-
-
-class ItemDescription: pass
-def list_player_items() -> List[ItemDescription]:
-   return []
-
-def give_player_item(item_name:str, item_description:str, physical_description:str) -> None:
-   pass
-
-def player_use_item(item_name:str, action_description:str, consume_amount:int=0) -> None:
-   pass
-
-
+from typing import List
 
 class Game:
    events: List[Event]
