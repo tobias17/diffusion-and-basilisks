e6b728a28ce9616718f07ae0f8e9dedebf99f51a
tobias17 <tobiasfischer17@gmail.com>
2024-08-14 19:56:37 -0400

    testing with model

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from prompts import Template, intro, state_map
 from functions import Functions
 
 from typing import List
+from openai import OpenAI
 
 class Game:
    events: List[Event]
@@ -25,6 +26,17 @@ def game_loop(game:Game):
       template = Template(intro, Functions.from_state(current_state), state_map[current_state])
       prompt = template.render()
       print(prompt)
+      client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
+      completion = client.chat.completions.create(
+         model="lmstudio-community/Meta-Llama-3.1-8B-Instruct-GGUF",
+         messages=[
+            { "role":"system", "content":prompt },
+         ],
+         temperature=0.7,
+         max_tokens=128,
+         # stop=["```", "$$end_"],
+      )
+      print(completion.choices[0].message.content)
       # game.add_event(StateTransitionEvent(from_state=current_state, to_state=State.HUB_IDLE))
    else:
       raise ValueError(f"game_loop() does not support {current_state} state yet")
