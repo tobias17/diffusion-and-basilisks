664660f4e8ae6591cc05beec355f62eaa57e3486
tobias17 <tobiasfischer17@gmail.com>
2024-08-14 19:33:15 -0400

    added examples to prompt

--- prompts.py
+++ prompts.py
@@ -34,17 +34,28 @@ default_world = """
 The game takes place in Iosla, a high fantasy realm full of mystery, dangers, and loot. A wide variety of creatures populate Iosla, both fantastic and degenerate.
 """.strip()
 
-intro = f"""
+intro = f'''
 You are a large language model tasked with helping a human play a video game. You will be playing the role of game master where you will be prompted to make meta-level decisions as well as generate individual bits of content.
 
 {default_world}
 
-Your interactions with the game world will be through an API where you will call python functions to generate content and make decisions.
+Your interactions with the game world will be through an API where you will call python functions to generate content and make decisions. The following is an example of how you might follow this API.
+
+$$begin_example_api$$
+def create_apple(color:str, physical_description:str) -> None:
+   """Creates a new apple with the given color and physical description"""
+$$end_example_api$$
+
+$$begin_code_example$$
+```python
+create_apple("red", "a Red Delicious apple, deep maroon skin, stem poking out of top, a slight glare of lighting")
+```
+$$end_code_example$$
 
 You will be fed information based on the current state of the game world. While there are a few, most fall under the HUB or TRAVEL umbrella.
 A Hub in this game is any combat-disabled location where the player is free to roam, inspect, and talk with other characters. This could be a town, village, outpost, or any other locations like that.
 The Travel states indicate the player traversing combat-enabled area, such as a road or a cave.
-""".strip()
+'''.strip()
 
 
 
@@ -53,6 +64,9 @@ state_map: Dict[State,str] = {}
 
 state_map[State.INITIALIZING] = f"""
 The player is currently in the INIALIZING state. Please call the create_hub function to generate the world's first hub.
+
+$$begin_code_block$$
+```python
 """.strip()
 
 ask_for_function_calls = """
