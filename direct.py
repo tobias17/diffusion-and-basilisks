c4665249d203817ae32ff3a050d8b9f32c33b220
Tobias Fischer <tobiasfischer17@gmail.com>
2025-02-15 09:52:21 -0500

    new API

--- direct.py
+++ direct.py
@@ -7,17 +7,15 @@ You are a helpful assistant who will play the role of Dungeon Master in a high f
 
 While this looks and functions like python, you only have access to the functions themselves and no higher-level programming functions. The following is the API you will have access to:
 ```
-game.create_town(name:str)
-game.create_background(id:str, desc:str)
-player.move_to_town(name:str, background:str)
-game.change_background(background:str)
-game.create_npc(name:str, desc:str)
-narrator.speak(text:str)
-player.speak_with_npc(name:str)
-game.get_npc(name:str).speak(text:str)
-game.give_quest(name:str, desc:str)
-game.get_quest(name:str).complete()
-game.create_item(name:str, desc:str).give_to_player()
+GAME.create_town(name:str) -> Town: pass
+GAME.make_new_background(id:str, desc:str) -> Background: pass
+GAME.get_background(id:str) -> Background: pass
+PLAYER.move_to(town:Town, background:Background) -> None: pass
+GAME.make_new_npc(name:str, desc:str) -> Character: pass
+GAME.get_npc(name:str) -> Character: pass
+Character.speak_to_player(text) -> None: pass
+PLAYER.speak_to_npc(npc:Character, text:str) -> None: pass
+NARRATOR.speak(text:str) -> None: pass
 ```
 
 Start off by create a new town and moving the player to it. You will then wait for the user to input what they would like to do.
@@ -26,9 +24,9 @@ Make sure to only advance the game by what is necessary to satisfy the user's re
 """.strip()
    }, {
 "role": "assistant", "content": """
-game.create_town(name="Iosla")
-game.create_background(id="iosla.town_center", desc="A charming seaside town centered around an ancient gnarled oak tree with massive spreading branches in the town square.")
-player.move_to_town(name="Iosla", background="iosla.town_center")
+iosla = GAME.create_town(name="Iosla")
+bg = GAME.make_new_background(id="iosla.town_center", desc="A charming seaside town centered around an ancient gnarled oak tree with massive spreading branches in the town square.")
+PLAYER.move_to(town=iosla, background=bg)
 """.strip()
    }, {
 "role": "user", "content": """
@@ -36,7 +34,7 @@ What kind of buildings surround me?
 """.strip()
    }, {
 "role": "assistant", "content": """
-narrator.speak("You look around and see many small houses, with a tavern a little ways down the road.")
+NARRATOR.speak("You look around and see many small houses, with a tavern a little ways down the road.")
 """.strip()
    }, {
 "role": "user", "content": """
