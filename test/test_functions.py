49ab7c23b20e2b06d752e13a90bb5980660be774
Tobias Fischer <tobiasfischer17@gmail.com>
2025-03-09 20:22:08 -0400

    removed old testing code

--- test/test_functions.py
+++ test/test_functions.py
@@ -51,39 +51,5 @@ class Test_Parse_Function(unittest.TestCase):
    def test_arg_after_kwarg(self):
       self.__sad('add_text(first="Hello,", " sailor!")')
 
-
-# function_pool = [
-#    Function((lambda s,a,b: a+b), "add_text", "", Parameter("first",str), Parameter("second",str)),
-#    Function((lambda s,a,b: a+b), "add_nums", "", Parameter("a",int), Parameter("b",int))
-# ]
-
-# class Test_Match_Function(unittest.TestCase):
-   
-#    def __happy(self, func_name:str, args:List, kwargs:Dict, expected_output):
-#       call, err = match_function(func_name, args, kwargs, function_pool)
-#       self.assertIsNotNone(call, err)
-#       assert call is not None
-#       out = call(None)
-#       self.assertEqual(out, expected_output)
-   
-#    def __sad(self, func_name:str, args:List, kwargs:Dict):
-#       call, err = match_function(func_name, args, kwargs, function_pool)
-#       self.assertIsNone(call, "Expected output to fail but got non-None value back")
-#       self.assertTrue(err, "Got None back but error message was empty")
-
-#    def test_simple_cast(self):
-#       self.__happy("add_text", ('"Hello,"', '" sailor!"'), {}, "Hello, sailor!")
-#    def test_int_cast(self):
-#       self.__happy("add_nums", ("5", "7"), {}, 12)
-#    def test_negative_int_cast(self):
-#       self.__happy("add_nums", ("-5", "7"), {}, 2)
-#    def test_pos_and_kwargs_mix(self):
-#       self.__happy("add_nums", ("5"), {"b": "7"}, 12)
-   
-#    def test_bad_int_cast(self):
-#       self.__sad("add_nums", ("5", "7.8"), {})
-#    def test_kwarg_before_pos_arg(self):
-#       self.__sad("add_nums", ("5"), {"a": "7"})
-
 if __name__ == "__main__":
    unittest.main()
